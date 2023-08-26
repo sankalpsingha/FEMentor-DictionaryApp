4def2db723453070d994dcc0fc7cef2de443b4b7
sankalpsingha <sankalp.singha@autobahn-security.com>
2023-08-27 04:17:13 +0700

    added the tanstack changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Header from "./components/Header";
 import SearchInput from "./components/SearchInput";
 import SearchResult from "./components/SearchResult";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <div className="absolute w-full h-full bg-cream dark:bg-black">
-      <div className="mx-auto max-w-3xl md:my-20 px-5 ">
-        <Header />
-        <SearchInput />
-        <SearchResult />
+    <QueryClientProvider client={queryClient}>
+      <div className="absolute w-full h-full bg-cream dark:bg-black">
+        <div className="mx-auto max-w-3xl md:my-20 px-5 ">
+          <Header />
+          <SearchInput />
+          <SearchResult />
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,16 +1,43 @@
 import React from "react";
+import axios from "axios";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
 
 const SearchInput = () => {
+  const queryClient = useQueryClient();
+  const [searchTerm, setSearchTerm] = React.useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("searching for : ", searchTerm);
+    setSearchTerm("");
+    mutation.mutate(searchTerm);
+  };
+
+  const getDictionarySearch = async (searchTerm: string) => {
+    return await axios.get(
+      "https://api.dictionaryapi.dev/api/v2/entries/en/" + searchTerm
+    );
+  };
+  const mutation = useMutation({
+    mutationFn: getDictionarySearch,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["searchResults"] });
+    },
+  });
   return (
     <div className="md:mt-10 relative">
-      <input
-        type="text"
-        className="w-full bg-offWhiteLight dark:bg-lighterBlack border-transparent focus:ring-0 focus:border-transparent focus:outline-none rounded-2xl pl-5 py-3 font-bold"
-        placeholder="Search"
-      />
-      <div className="absolute top-4 right-5">
-        <img src="/search-icon.svg" className="w-5 h-5" alt="search-icon" />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="w-full bg-offWhiteLight dark:bg-lighterBlack border-transparent focus:ring-0 focus:border-transparent focus:outline-none rounded-2xl pl-5 py-3 font-bold dark:text-white"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <div className="absolute top-4 right-5">
+          <img src="/search-icon.svg" className="w-5 h-5" alt="search-icon" />
+        </div>
+      </form>
     </div>
   );
 };
